d47ce312840decc5a09487b55558ad9e64dda6d6
hejian <flystarhe@qq.com>
2020-05-14 16:25:46 +0800

    Update pycls/core/

---
+++ pycls/core/builders.py
@@ -0,0 +1,60 @@
+import torch
+from pycls.core.config import cfg
+from pycls.models.anynet import AnyNet
+from pycls.models.effnet import EffNet
+from pycls.models.regnet import RegNet
+from pycls.models.resnet import ResNet
+
+
+# Supported models
+_models = {'anynet': AnyNet, 'effnet': EffNet, 'resnet': ResNet, 'regnet': RegNet}
+
+# Supported loss functions
+_loss_funs = {'cross_entropy': torch.nn.CrossEntropyLoss}
+
+
+def get_model():
+    '''Gets the model class specified in the config.'''
+    err_str = 'Model type "{}" not supported'
+    assert cfg.MODEL.TYPE in _models.keys(), err_str.format(cfg.MODEL.TYPE)
+    return _models[cfg.MODEL.TYPE]
+
+
+def get_loss_fun():
+    '''Gets the loss function class specified in the config.'''
+    err_str = 'Loss function type "{}" not supported'
+    assert cfg.MODEL.LOSS_FUN in _loss_funs.keys(), err_str.format(cfg.TRAIN.LOSS)
+    return _loss_funs[cfg.MODEL.LOSS_FUN]
+
+
+def build_model():
+    '''Builds the model.'''
+    # Construct the model
+    model = get_model()()
+    # Transfer the model to the current GPU device
+    err_str = 'Cannot use more GPU devices than available'
+    assert cfg.NUM_GPUS <= torch.cuda.device_count(), err_str
+    cur_device = torch.cuda.current_device()
+    model = model.cuda(device=cur_device)
+    # Use multi-process data parallel model in the multi-gpu setting
+    if cfg.NUM_GPUS > 1:
+        # Make model replica operate on the current device
+        model = torch.nn.parallel.DistributedDataParallel(
+            module=model, device_ids=[cur_device], output_device=cur_device
+        )
+    return model
+
+
+def build_loss_fun():
+    '''Build the loss function.'''
+    return get_loss_fun()().cuda()
+
+
+def register_model(name, ctor):
+    '''Registers a model dynamically.'''
+    _models[name] = ctor
+
+
+def register_loss_fun(name, ctor):
+    '''Registers a loss function dynamically.'''
+    _loss_funs[name] = ctor
