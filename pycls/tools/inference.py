c57e76d08d163b8f328221b7bc00910096c8ad3d
hejian <hejian@>
2020-08-25 17:12:06 +0800

    Bug pycls/tools/inference.py#mkdir

--- pycls/tools/inference.py
+++ pycls/tools/inference.py
@@ -177,6 +177,7 @@ def test():
         outputs.append([im_path, class_ids[a], class_ids[b], c])
 
     task_name = time.strftime("%m%d%H%M")
+    os.makedirs(os.path.join(cfg.OUT_DIR, task_name))
 
     temp_file = "{}/threshold.png".format(task_name)
     temp_file = os.path.join(cfg.OUT_DIR, temp_file)
