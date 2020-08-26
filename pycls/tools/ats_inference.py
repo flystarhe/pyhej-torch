fbce21e978a65a68f87902353e9fad2d22be7253
hejian <hejian@>
2020-08-26 16:52:15 +0800

    Fixed pycls/tools/ats_inference.py

--- pycls/tools/ats_inference.py
+++ pycls/tools/ats_inference.py
@@ -66,9 +66,9 @@ def setup_model():
 
 def search_thr(data, s1_thr=5, s2_thr=80, out_file=None):
     """Search from: [(label,pred_label,pred_score),]"""
-    def test_thr(label, pred_label, pred_score):
-        y_ = [b if a == 0 else 1 - b for a, b in zip(pred_label, pred_score)]
-        y, y_ = np.array(label, dtype="float32"), np.array(y_, dtype="float32")
+    def test_thr(label, score):
+        y = np.array(label, dtype="float32")
+        y_ = np.array(score, dtype="float32")
 
         x = np.linspace(0, 1, 100, endpoint=False)[1:]
         s1, s2 = np.zeros_like(x), np.zeros_like(x)
@@ -81,8 +81,8 @@ def search_thr(data, s1_thr=5, s2_thr=80, out_file=None):
             s2[i] = (num_tn / total_false) * 100
         return x, s1, s2
 
-    label, pred_label, pred_score = zip(*data)
-    x, s1, s2 = test_thr(label, pred_label, pred_score)
+    label, score = zip(*data)
+    x, s1, s2 = test_thr(label, score)
 
     inds = (s1 < s1_thr) * (s2 > s2_thr)
     if np.any(inds):
@@ -114,7 +114,7 @@ def search_thr(data, s1_thr=5, s2_thr=80, out_file=None):
     x, s1, s2 = x[best_id], s1[best_id], s2[best_id]
 
     print("threshold(x={:.2f},s1={:.2f},s2={:.2f})".format(x, s1, s2))
-    return {"false": x, "true": 1.0 - x}
+    return x
 
 
 def hardmini(outputs, class_ids, task_name, score_thr=None):
@@ -122,20 +122,19 @@ def hardmini(outputs, class_ids, task_name, score_thr=None):
     out_dir = os.path.join(cfg.OUT_DIR, out_dir)
 
     if score_thr is None:
-        score_thr = dict()
+        score_thr = 0.5
 
-    for ori_label in class_ids:
-        for out_label in class_ids:
-            sub_dir = os.path.join(out_dir, ori_label + out_label + "_U")
-            os.makedirs(sub_dir, exist_ok=True)
-            sub_dir = os.path.join(out_dir, ori_label + out_label + "_D")
-            os.makedirs(sub_dir, exist_ok=True)
+    for class_id in class_ids:
+        sub_dir = os.path.join(out_dir, class_id + "_true")
+        os.makedirs(sub_dir, exist_ok=True)
+        sub_dir = os.path.join(out_dir, class_id + "_false")
+        os.makedirs(sub_dir, exist_ok=True)
 
-    for im_path, ori_label, out_label, out_score in outputs:
-        if out_score >= score_thr.get(out_label, 0.5):
-            sub_dir = os.path.join(out_dir, ori_label + out_label + "_U")
+    for im_path, class_id, score in outputs:
+        if score < score_thr:
+            sub_dir = os.path.join(out_dir, class_id + "_true")
         else:
-            sub_dir = os.path.join(out_dir, ori_label + out_label + "_D")
+            sub_dir = os.path.join(out_dir, class_id + "_false")
         shutil.copyfile(im_path, os.path.join(sub_dir, os.path.basename(im_path)))
     return out_dir
 
@@ -161,13 +160,9 @@ def test():
         # Compute the predictions
         preds = model(inputs)
         preds = F.softmax(preds, dim=1)
-        # Find the top max_k predictions for each sample
-        topk_vals, topk_inds = torch.topk(preds, 1, dim=1)
-        # (batch_size, max_k) -> (max_k, batch_size)
-        topk_inds, topk_vals = topk_inds.t(), topk_vals.t()
-        repk_labels = labels.view(1, -1).expand_as(topk_inds)
-        for a, b, c in zip(repk_labels.tolist()[0], topk_inds.tolist()[0], topk_vals.tolist()[0]):
-            logs.append([a, b, c])
+        # (batch_size, classes) -> (classes, batch_size)
+        for label, score in zip(labels.tolist(), preds.t().tolist()[0]):
+            logs.append([label, score])
 
     imgs = [v["im_path"] for v in dataset.get_imdb()]
     class_ids = dataset.get_class_ids()
@@ -176,12 +171,12 @@ def test():
     lines = []
     outputs = []
     lines.append(":".join(class_ids))
-    lines.append("\nimages,{}\n".format(len(imgs)))
-    lines.append("im_path,label,pred_label,pred_score")
+    lines.append("{}".format(len(imgs)))
+    lines.append("image_path,label,score")
 
-    for im_path, (a, b, c) in zip(imgs, logs):
-        lines.append("{},{},{},{}".format(im_path, a, b, c))
-        outputs.append([im_path, class_ids[a], class_ids[b], c])
+    for im_path, (label, score) in zip(imgs, logs):
+        lines.append("{},{},{}".format(im_path, label, score))
+        outputs.append([im_path, class_ids[label], score])
 
     task_name = time.strftime("%m%d%H%M")
     os.makedirs(os.path.join(cfg.OUT_DIR, task_name))
